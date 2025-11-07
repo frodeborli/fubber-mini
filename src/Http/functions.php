f3f8dd5337ace7ffe541ef908f57c720f920a094
Frode Børli <frode@ennerd.com>
2025-11-07 00:19:09 +0000

    Add PSR-7/PSR-15/PSR-17 HTTP message support
    Dependencies Added:
    - psr/http-message: ^1.0 || ^2.0 (PSR-7 HTTP messages)
    - psr/http-server-handler: ^1.0 (PSR-15 request handlers)
    - psr/http-server-middleware: ^1.0 (PSR-15 middleware)
    - psr/http-factory: ^1.0 (PSR-17 HTTP factories)
    - nyholm/psr7: ^1.8 (Nyholm PSR-7 implementation)
    - nyholm/psr7-server: ^1.1 (ServerRequest from globals)
    
    Helper Functions Added (mini\Http namespace):
    - create_request_from_globals(): Create PSR-7 ServerRequest from PHP globals
    - create_response(status, body): Create PSR-7 Response
    - create_json_response(data, status): Create JSON response
    - emit_response(ResponseInterface): Send response to client
    
    Philosophy:
    PSR-7 is designed for request/response flow, not as services.
    Request handlers receive ServerRequestInterface and return ResponseInterface.
    These are just convenience functions - direct Nyholm usage is also fine.

--- src/Http/functions.php
+++ src/Http/functions.php
@@ -2,9 +2,87 @@
 
 namespace mini\Http;
 
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Nyholm\Psr7Server\ServerRequestCreator;
+
 /**
- * HTTP Feature - Service Registration
+ * HTTP Feature - PSR-7 Helper Functions
  *
- * This file is intentionally minimal.
- * HTTP exceptions (HttpException, NotFoundException, etc.) are autoloaded via PSR-4.
+ * Provides convenience functions for working with PSR-7 HTTP messages.
+ * Uses Nyholm's PSR-7 implementation.
  */
+
+/**
+ * Create ServerRequest from PHP globals
+ *
+ * @return ServerRequestInterface
+ */
+function create_request_from_globals(): ServerRequestInterface {
+    $psr17Factory = new Psr17Factory();
+    $creator = new ServerRequestCreator(
+        $psr17Factory,  // ServerRequestFactory
+        $psr17Factory,  // UriFactory
+        $psr17Factory,  // UploadedFileFactory
+        $psr17Factory   // StreamFactory
+    );
+
+    return $creator->fromGlobals();
+}
+
+/**
+ * Create a PSR-7 Response
+ *
+ * @param int $status HTTP status code
+ * @param string $body Response body
+ * @return ResponseInterface
+ */
+function create_response(int $status = 200, string $body = ''): ResponseInterface {
+    $factory = new Psr17Factory();
+    $response = $factory->createResponse($status);
+
+    if ($body !== '') {
+        $response = $response->withBody($factory->createStream($body));
+    }
+
+    return $response;
+}
+
+/**
+ * Create a JSON response
+ *
+ * @param mixed $data Data to encode as JSON
+ * @param int $status HTTP status code
+ * @return ResponseInterface
+ */
+function create_json_response(mixed $data, int $status = 200): ResponseInterface {
+    $json = json_encode($data, JSON_THROW_ON_ERROR);
+    return create_response($status, $json)->withHeader('Content-Type', 'application/json');
+}
+
+/**
+ * Send a PSR-7 Response to the client
+ *
+ * @param ResponseInterface $response
+ * @return void
+ */
+function emit_response(ResponseInterface $response): void {
+    // Send status line
+    header(sprintf(
+        'HTTP/%s %s %s',
+        $response->getProtocolVersion(),
+        $response->getStatusCode(),
+        $response->getReasonPhrase()
+    ), true, $response->getStatusCode());
+
+    // Send headers
+    foreach ($response->getHeaders() as $name => $values) {
+        foreach ($values as $value) {
+            header(sprintf('%s: %s', $name, $value), false);
+        }
+    }
+
+    // Send body
+    echo $response->getBody();
+}
